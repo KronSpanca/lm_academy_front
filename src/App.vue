f72e494eccac765afe50080f41f488931e8b3291
KronSpanca <kronspanca61@gmail.com>
2025-03-10 19:02:45 +0100

    Installed vuejs

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+<script setup>
+import {ref} from 'vue'
+const message = ref('Pershendetje')
+
+setTimeout(() => {
+  message.value = 'Diten e Mire'
+}, 5000)
+</script>
+<style>
+ h1{
+  color: gold;
+ }
+</style>
